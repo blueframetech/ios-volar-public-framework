56aba32846541cea52a875061b71be7cd294580b
chris-allen <chris@volarvideo.com>
2014-09-09 15:06:18 -0400

    Updated docs for release

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['broadcastparams',['BroadcastParams',['../interface_broadcast_params.html',1,'']]],
-  ['bundleutil',['BundleUtil',['../interface_bundle_util.html',1,'']]]
+  ['broadcastparams',['BroadcastParams',['../interface_broadcast_params.html',1,'']]]
 ];
