661bebd3195f5d238ee62dc44df039f2a43e2db4
chris-allen <chris@volarvideo.com>
2014-09-24 11:11:08 -0400

    Updated for v2.0.1

--- search/functions_4.js
+++ search/functions_4.js
@@ -5,5 +5,6 @@ var searchData=
   ['initwithextendedvmapuristring_3a',['initWithExtendedVMAPURIString:',['../interface_v_v_movie_player_view_controller.html#ae1d1cacacf8c79963fb85c969300136e',1,'VVMoviePlayerViewController']]],
   ['initwithextendedvmapuristring_3aautoplay_3a',['initWithExtendedVMAPURIString:autoPlay:',['../interface_v_v_movie_player_controller.html#ae181949997bb5931d41be78f8f8578ce',1,'VVMoviePlayerController']]],
   ['initwithextendedvmapuristring_3awithapi_3a',['initWithExtendedVMAPURIString:withAPI:',['../interface_v_v_movie_player_view_controller.html#a87ce3375d85454507bde961d898e45f8',1,'VVMoviePlayerViewController']]],
-  ['initwithextendedvmapuristring_3awithapi_3aautoplay_3a',['initWithExtendedVMAPURIString:withAPI:autoPlay:',['../interface_v_v_movie_player_controller.html#a6579bb5b24a31656bc49f793c8e45d73',1,'VVMoviePlayerController']]]
+  ['initwithextendedvmapuristring_3awithapi_3aautoplay_3a',['initWithExtendedVMAPURIString:withAPI:autoPlay:',['../interface_v_v_movie_player_controller.html#a6579bb5b24a31656bc49f793c8e45d73',1,'VVMoviePlayerController']]],
+  ['isscaledbuttonhidden',['isScaledButtonHidden',['../interface_v_v_movie_player_controller.html#a3d9da8bd02355ccb14c4a65c25b6ac79',1,'VVMoviePlayerController']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['setfullscreen_3aanimated_3a',['setFullscreen:animated:',['../interface_v_v_movie_player_controller.html#aed70ff2700a055babe3d4252628d6367',1,'VVMoviePlayerController']]],
+  ['setscaledbuttonhidden_3a',['setScaledButtonHidden:',['../interface_v_v_movie_player_controller.html#a51b9eaa62e4c7faaae2c962edc23a740',1,'VVMoviePlayerController']]],
   ['shouldautorotatetointerfaceorientation_3a',['shouldAutorotateToInterfaceOrientation:',['../interface_v_v_movie_player_view_controller.html#ada59a64896690dabbd3abe86e5e2e232',1,'VVMoviePlayerViewController']]]
 ];
